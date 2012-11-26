4ae67952a7ae00685562f7812ff7db8df10e3a51
Sapronov Alex <warmonger72@gmail.com>
2012-11-27 00:10:34 +0700

    [UPDATE] update gui

--- src/defines/defines.h
+++ src/defines/defines.h
@@ -2,9 +2,9 @@
 #define __DEFINES_H
 
 #define GL_PROG_VERSION_STR	"0.0"
-#define GL_PROG_BUILD_STR	"2012.08.2"
-#define GL_PROG_NAME "untitled"
+#define GL_PROG_BUILD_STR	"2012.11.27"
+#define GL_PROG_NAME "QNoisyImages"
 #define GL_PROG_AUTHOR "Alexander Sapronov"
-#define GL_WEB_SITE "http://warmonger1.github.com/bQella/"
+#define GL_WEB_SITE "https://github.com/WarmongeR1/QNoisyImages/"
 
 #endif // __DEFINES_H
